6ca4413571c8f7ef393d1663bc8887d65153d977
tpphu <vietean@gmail.com>
2015-04-11 22:31:26 +0700

    Init project

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path 	= require('path');
+var app 	= express();
+var server 	= require('http').Server(app);
+var io 		= require('socket.io')(server);
+
+
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
+
+server.listen(3000);
