79e1e04e5fed838d1bc3271d784ffb4152028e24
tpphu <vietean@gmail.com>
2015-04-17 22:31:59 +0700

    + Van de voi transport

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require('express');
 var path 	= require('path');
 var app 	= express();
 var server 	= require('http').Server(app);
-var io 		= require('socket.io')(server);
 
+var io 		= require('socket.io')(server,{
+	'transports' : ['websocket', 'polling', 'xhr-polling']
+});
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
-
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
